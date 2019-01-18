9a363e9b93ed9ac4271f3ae675f6a2474ece6df8
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-01-17 20:53:33 -0500

    added in comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int finalNum = 1000;
+        int finalNum = 1000; //stopping point
         int multA = 3;
         int multB = 5;
         int sum = 0;
 
 
+        //loop starts at 1 and ends at finalNum (1000)
         for (int num = 1; num < finalNum; num++){
+            //checks if num is evenly divisible by multA (3) and multB(5)
             if ((num%multA)==0 || (num%multB)==0) {
-                sum += num;
+                sum += num; //if condition is met num is added to sum
             }
         }
 
